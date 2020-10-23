a27bb66bd4d24a1d16413df997b9ddc3576fb30f
proiacm <proiacm@gmail.com>
2020-10-23 12:52:19 -0600

    Output character validation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
-import logo from './logo.svg';
+import React, {Component} from 'react'
 import './App.css';
+import Validation from './Validation/Validation';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+  state = {
+    text: '',
+    textLength: ''
+  }
+
+  lengthHandler = (event) => {
+    // console.log(event.target.value.length)
+    let text = {...this.state.textLength}
+    text = event.target.value.length
+    this.setState({textLength: text})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type='text' onChange={(event) => this.lengthHandler(event)}/>
+        <p>{this.state.textLength}</p>
+        <Validation length={this.state.textLength}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/Validation/Validation.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Validation = (props) => {
+
+    let message;
+    if(props.length < 5 && props.length != 0){
+        message = 'Text is too short'
+    }else if(props.length > 20){
+        message = 'Text is too long'
+    }
+
+    return (
+        <div>
+            {message}
+        </div>
+    )
+}
+
+export default Validation;
